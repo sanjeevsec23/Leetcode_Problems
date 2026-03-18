d6258e702b6a743c3ba797ad9b5dc895e0d321ca
sanjeevsec23 <sanjeevs.ec23@bitsathy.ac.in>
2026-03-18 15:04:06 +0530

    Time: 1042 ms (40.51%) | Memory: 8.5 MB (31.21%) - LeetSync

---
+++ 507-perfect-number/perfect-number.c
@@ -0,0 +1,14 @@
+bool checkPerfectNumber(int num) {
+    long int sum=0;
+    for(int i=1;i<=num/2;i++){
+        if(num%i==0){
+            sum=sum+i;
+        }
+    }
+    if(sum == num){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
